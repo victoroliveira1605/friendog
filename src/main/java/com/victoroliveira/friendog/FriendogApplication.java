9a8c6c3b2672bb24c51b2286a17d5cda4f5064d6
Victor Oliveira <victoroliveira.160595@gmail.com>
2019-02-21 10:08:46 -0300

    Project created

---
+++ src/main/java/com/victoroliveira/friendog/FriendogApplication.java
@@ -0,0 +1,13 @@
+package com.victoroliveira.friendog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/victoroliveira/friendog/FriendogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.victoroliveira.friendog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FriendogApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
